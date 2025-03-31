changes to kernel size, added more plots, and fixed an off by one error

main-model/data_loader.py
@@ -193,7 +193,6 @@ class EquityDataset(Dataset):
         
         train_data = []
         val_data = []
-        test_data = []
 
         designee_tickers = self.technology_tickers if industry == "technology" else self.energy_tickers if industry == "energy" else self.agriculture_tickers
 
@@ -211,24 +210,20 @@ class EquityDataset(Dataset):
             df = df.sort_values(by="Date")
             
             # Define split points (e.g., 80% train, 10% val, 10% test)
-            train_idx = int(len(df) * 0.8)
-            val_idx = int(len(df) * 0.9)
+            train_idx = int(len(df) * 0.9)
             
             # Split data chronologically
             train_df = df.iloc[:train_idx]
-            val_df = df.iloc[train_idx:val_idx]
-            test_df = df.iloc[val_idx:]
+            val_df = df.iloc[train_idx:]
             
             # Create samples for each split
             train_data.extend(self.split_df_into_samples(train_df, sample_stride=sample_stride))
             val_data.extend(self.split_df_into_samples(val_df, sample_stride=sample_stride))
-            test_data.extend(self.split_df_into_samples(test_df, sample_stride=sample_stride))
             
         # Create DataLoader objects
         self.train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
         self.val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=False)
-        self.test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
-        print(f"Train samples: {len(train_data)}, Val samples: {len(val_data)}, Test samples: {len(test_data)}")
+        print(f"Train samples: {len(train_data)}, Val samples: {len(val_data)}")
         # Save the data loaders for later use
         if not os.path.exists("./cached_data_loaders"):
             os.makedirs("./cached_data_loaders")
@@ -262,13 +257,52 @@ class EquityDataset(Dataset):
             # convert the sample to a tensor
             sample = torch.tensor(sample.values, dtype=torch.float32)
             # make it an input, target pair
-            input_sample = sample[:self.input_height, 1:]
-            target_sample = sample[self.input_height:, 1:]
+            input_sample = sample[:self.input_height, :]
+            target_sample = sample[self.input_height:, :]
             # add the sample to the list
             samples.append((input_sample, target_sample))  # Add channel dimension for target
         # return the samples as a list of tuples
         return samples
         
+    def get_recent_input_tensror_for_ticker(self, ticker, industry="technology"):
+        if industry not in ["technology", "energy", "agriculture"]:
+            raise ValueError("Invalid industry specified. Choose from 'technology', 'energy', or 'agriculture'.")
+        designee_tickers = self.technology_tickers if industry == "technology" else self.energy_tickers if industry == "energy" else self.agriculture_tickers
+        if ticker not in designee_tickers:
+            raise ValueError(f"Ticker not found in dataset for {industry}")
+        
+        df = self.dataframes_dict[ticker]
+        # Get the most recent sample
+        recent_sample = df.iloc[-self.sample_window:]
+        # Drop the date column
+        recent_sample = recent_sample.drop(columns=["Date"])
+        # Normalize the sample based on the first input_height values
+        avg_max = 0
+        avg_min = 0
+        divisor = 0
+        for col in recent_sample.columns:
+            
+            min_val = recent_sample[col][:self.input_height].min()
+            max_val = recent_sample[col][:self.input_height].max()
+
+            if col in ["Open", "High", "Low", "Close"]:
+                avg_max += max_val
+                avg_min += min_val
+                divisor += 1
+                
+            if max_val - min_val != 0:
+                recent_sample[col] = (recent_sample[col] - min_val) / (max_val - min_val)
+            else:
+                recent_sample[col] = 0.5
+
+        avg_max /= divisor
+        avg_min /= divisor
+        # Convert the sample to a tensor
+        recent_sample = torch.tensor(recent_sample.values, dtype=torch.float32)
+        # split into input and target
+        input_sample = recent_sample[:self.input_height, 0:]
+        target_sample = recent_sample[self.input_height:, 0:]
+        return ((input_sample, target_sample), avg_max, avg_min)
     
     def plot_and_save_image(self, ticker, save_path="./dataset_plots"):
 

main-model/model.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class EquityModel(nn.Module):
-    def __init__(self, width_k_bar=8, kernel_size=3, input_height=256, output_height=16, conv_out_channel_list=[4, 16, 64, 256], pool_type='avg'):
+    def __init__(self, width_k_bar=18, kernel_size=3, input_height=256, output_height=16, conv_out_channel_list=[4, 16, 64, 256], pool_type='avg'):
         super(EquityModel, self).__init__()
 
         self.conv_layers = []
@@ -11,10 +11,10 @@ class EquityModel(nn.Module):
         # Create convolutional layers
         for i in range(len(conv_out_channel_list)):
             if i == 0:
-                self.conv_layers.append(nn.Conv2d(1, conv_out_channel_list[i], kernel_size, padding=1, stride=1))
+                self.conv_layers.append(nn.Conv2d(1, conv_out_channel_list[i], kernel_size, padding=(2, 2), stride=1))
             else:
-                self.conv_layers.append(nn.Conv2d(conv_out_channel_list[i-1], conv_out_channel_list[i], kernel_size, padding=1, stride=1))
-        self.final_conv_layer = nn.Conv2d(conv_out_channel_list[-1], 1, kernel_size, padding=1, stride=1) # Final conv layer to reduce channels to 1
+                self.conv_layers.append(nn.Conv2d(conv_out_channel_list[i-1], conv_out_channel_list[i], kernel_size, padding=(2,2), stride=1))
+        self.final_conv_layer = nn.Conv2d(conv_out_channel_list[-1], 1, kernel_size-2, padding=1, stride=1) # Final conv layer to reduce channels to 1
 
         self.conv_layers = nn.ModuleList(self.conv_layers)
         self.width_k_bar = width_k_bar
@@ -40,21 +40,21 @@ class EquityModel(nn.Module):
         for conv in self.conv_layers:
 
             x = conv(x)
+
             # x = F.relu(x) # Perhaps try other activations like LeakyReLU, or just linear
-            x = F.dropout(x, p=0.5) # if dropout is needed to prevent overfitting we can add it here
+            x = F.dropout(x, p=0.65) # if dropout is needed to prevent overfitting we can add it here
         
             # Pooling layer
             if self.pool_type == 'avg':
                 x = F.avg_pool2d(x, kernel_size=(2, 1), stride=(2, 1), padding=(0, 0))
             elif self.pool_type == 'max':
                 x = F.max_pool2d(x, kernel_size=(2, 1), stride=(2, 1), padding=(0, 0))
-            else:
+            else: 
                 raise ValueError("pool_type must be 'avg' or 'max', got {}".format(self.pool_type))
             
         x = self.final_conv_layer(x)
 
         x = x.squeeze(1) # remove channel dimension, x.shape = (batch_size, output_height, width_k_bar)
-        
         return x # x.shape = (batch_size, output_height, width_k_bar)
     
     

main-model/train.py
@@ -37,18 +37,14 @@ def get_device():
         return "mps"
     else:
         return "cpu"
-    
+device = get_device()
+
 
 def avishit_model_accuracy(model, data_loader, output_height=1):
     return 0
     
 def get_model_accuracy(model, data_loader, output_height=1):
-    device = get_device()
     model.to(device)
-    model.eval()
-
-    # Run a simulations where we buy if the model predicts a price increase and sell if it predicts a price decrease
-    # and calculate the accuracy of the model as the % return of the simulation
     total_return = 0.0
     total_trades = 0
     correct_trades = 0
@@ -58,21 +54,45 @@ def get_model_accuracy(model, data_loader, output_height=1):
         with torch.no_grad():
             outputs = model(inputs)
 
-        
+        for i, output in enumerate(outputs):
+            for day in range(output_height):
+                todays_prices = inputs[i][-1, :4].squeeze(0)
+                tomorrows_prices = output[day, :4].squeeze(0)
+                if day != 0:
+                    todays_prices = targets[i][day-1, :4].squeeze(0)
+                true_prices = targets[i][day, :4].squeeze(0)
+                # average price of the first 4 channels
+                todays_avg_price = (todays_prices[0] + todays_prices[1] + todays_prices[2] + todays_prices[3]) / 4
+                tomorrows_avg_price = (tomorrows_prices[0] + tomorrows_prices[1] + tomorrows_prices[2] + tomorrows_prices[3]) / 4
+                true_avg_price = (true_prices[0] + true_prices[1] + true_prices[2] + true_prices[3]) / 4
+                
+                if tomorrows_avg_price > todays_avg_price and true_avg_price > todays_avg_price:
+                    correct_trades += 1
+                elif tomorrows_avg_price < todays_avg_price and true_avg_price < todays_avg_price:
+                    correct_trades += 1
+                elif tomorrows_avg_price == true_avg_price:
+                    correct_trades += 1
+                elif true_avg_price == todays_avg_price:
+                    correct_trades += 1
+                
+                total_trades += 1
+        if total_trades > 10000: 
+            break
+                
+    return (correct_trades / total_trades)
 
-
-def train_model(model, train_loader, val_loader, num_epochs=10, lr=0.0001, save_interval=10, output_height=1):
-    device = get_device()
+def train_model(model, train_loader, val_loader, num_epochs=10, lr=0.0001, output_height=1):
     model.to(device)
     criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=lr)
 
     training_losses = []
     validation_losses = []
-    training_accuracies = []
     validation_accuracies = []
+    training_accuracies = []
 
     best_val_loss = float('inf')
+    best_val_accuracy = 0.0
 
     for epoch in range(num_epochs):
         model.train()
@@ -90,7 +110,8 @@ def train_model(model, train_loader, val_loader, num_epochs=10, lr=0.0001, save_
             optimizer.step()
             running_loss += loss.item()
         training_losses.append(running_loss / len(train_loader))
-        # Validation
+        training_accuracies.append(get_model_accuracy(model, train_loader, output_height=output_height))
+
         model.eval()
         val_loss = 0.0
         with torch.no_grad():
@@ -100,30 +121,37 @@ def train_model(model, train_loader, val_loader, num_epochs=10, lr=0.0001, save_
                 loss = criterion(outputs, targets)
                 val_loss += loss.item()
         validation_losses.append(val_loss / len(val_loader))
+        validation_accuracies.append(get_model_accuracy(model, val_loader, output_height=output_height))
 
         if validation_losses[-1] < best_val_loss:
             best_val_loss = validation_losses[-1]
             # Save the model if validation loss improves
             if not os.path.exists("./cached_models"):
                 os.makedirs("./cached_models")
-            torch.save(model.state_dict(), "./cached_models/best_equitymodel.pth")
-            print(f"Model saved at epoch {epoch+1} with validation loss: {best_val_loss:.4f}")
+            torch.save(model.state_dict(), "./cached_models/best_loss_equitymodel.pth")
+        if validation_accuracies[-1] > best_val_accuracy:
+            best_val_accuracy = validation_accuracies[-1]
+            # Save the model if validation accuracy improves
+            if not os.path.exists("./cached_models"):
+                os.makedirs("./cached_models")
+            torch.save(model.state_dict(), "./cached_models/best_accuracy_equitymodel.pth")
         
         # Print statistics
-        print(f"Epoch {epoch+1} | Training Loss: {training_losses[-1]:.4f} | Validation Loss: {validation_losses[-1]:.4f}")
+        print(f"Epoch [{epoch+1}] | Training Loss: {training_losses[-1]:.4f} | Validation Loss: {validation_losses[-1]:.4f} | Validation Accuracy: {validation_accuracies[-1]:.4f} | Training Accuracy: {training_accuracies[-1]:.4f}")
 
-    return training_losses, validation_losses, training_accuracies, validation_accuracies
+    return training_losses, validation_losses, validation_accuracies, training_accuracies
         
 
-def main():
+def main(train=True):
     # note that the input height muse be 256 times the output height
 
-    convolutional_layers = [8, 64, 256, 32, 4]
-    o_ht = 8
+    # convolutional_layers = [4, 16, 64, 256, 32, 16, 8, 4] # this works well
+    convolutional_layers = [4, 8, 32, 128, 512, 64, 16, 4]
+    o_ht = 1
     i_ht = o_ht * (2**len(convolutional_layers))
     eqds = EquityDataset(input_height=i_ht, output_height=o_ht, include_industry_specific=True, normalize=False)
     train_loader, val_loader, test_loader = eqds.construct_data_loaders(industry="technology", sample_stride=4, batch_size=32)
-    model = EquityModel(width_k_bar=17, kernel_size=3, input_height=i_ht, output_height=o_ht, conv_out_channel_list=convolutional_layers, pool_type='avg')
+    model = EquityModel(width_k_bar=18, kernel_size=5, input_height=i_ht, output_height=o_ht, conv_out_channel_list=convolutional_layers, pool_type='avg')
     
     # print(train_loader.dataset[0][0].shape)
     # show_tensor_image(train_loader.dataset[0][0], title="First Image in Training Set")
@@ -135,19 +163,97 @@ def main():
     # print("Validation set size:", len(val_loader.dataset))
     # print("Test set size:", len(test_loader.dataset))
 
-    tl, vl, ta, va = train_model(model, train_loader, val_loader, num_epochs=30, lr=0.0001, output_height=o_ht)
-    # Plot training and validation loss
-    plt.figure(figsize=(10, 5))
-    plt.plot(tl, label='Training Loss', color='blue')
-    plt.plot(vl, label='Validation Loss', color='red')
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.title('Training and Validation Loss')
-    plt.legend()
-    plt.grid()
+    if train:
+        tl, vl, va, ta = train_model(model, train_loader, val_loader, num_epochs=10, lr=0.0001, output_height=o_ht)
+        # Plot training and validation loss
+        plt.figure(figsize=(10, 5))
+        plt.plot(tl, label='Training Loss', color='blue')
+        plt.plot(vl, label='Validation Loss', color='red')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.title('Training and Validation Loss')
+        plt.legend()
+        plt.grid()
+        plt.show()
+        # Plot training and validation accuracy
+        plt.figure(figsize=(10, 5))
+        plt.plot(va, label='Validation Accuracy', color='red')
+        plt.plot(ta, label='Training Accuracy', color='blue')
+        plt.xlabel('Epoch')
+        plt.ylabel('Accuracy')
+        plt.title('Training and Validation Accuracy')
+        plt.legend()
+        plt.grid()
+        plt.show()
+
+    best_model = torch.load("./cached_models/best_loss_equitymodel.pth")
+    model.load_state_dict(best_model)
+
+    # test_accuracy = get_model_accuracy(model, test_loader, output_height=o_ht)
+    # print(f"Test Accuracy: {100*test_accuracy}%")
+
+    # Do another quick test on the last sample window days of a stock
+    data, _max, _min = eqds.get_recent_input_tensror_for_ticker("AAPL")
+
+    model.to(device)
+    prediction = model(data[0].unsqueeze(0).to(device))
+
+    # un-normalize the prediction and actual values
+    input_tensor = data[0].cpu().detach().numpy()[:,:4]
+    input_tensor = input_tensor * (_max - _min) + _min
+    prediction = prediction.cpu().detach().numpy().squeeze(0)[:,:4]
+    prediction = prediction * (_max - _min) + _min
+    truth = data[1].cpu().detach().numpy()[:, :4]
+    truth = truth * (_max - _min) + _min
+
+    prediction = np.concatenate((input_tensor, prediction), axis=0)
+    input_tensor = np.concatenate((input_tensor, truth), axis=0)
+
+    # Show the prediction vs truth in 4 subplots corresponding to the 4 channels, with predicion as large dots
+    _, axs = plt.subplots(2, 2, figsize=(10, 10))
+    axs[0, 0].plot(prediction[:,0], label='Prediction', color='red')
+    axs[0, 0].plot(input_tensor[:,0], label='Actual', color='indigo')
+    axs[0, 0].plot(input_tensor[:len(input_tensor)-1,0], label='Actual (Past)', color='blue')
+
+    axs[0, 1].plot(prediction[:,1], label='Prediction', color='red')
+    axs[0, 1].plot(input_tensor[:,1], label='Actual', color='indigo')
+    axs[0, 1].plot(input_tensor[:len(input_tensor)-1,1], label='Actual (Past)', color='blue')
+
+    axs[1, 0].plot(prediction[:,2], label='Prediction', color='red')
+    axs[1, 0].plot(input_tensor[:,2], label='Actual', color='indigo')
+    axs[1, 0].plot(input_tensor[:len(input_tensor)-1,2], label='Actual (Past)', color='blue')
+
+    axs[1, 1].plot(prediction[:,3], label='Prediction', color='red')
+    axs[1, 1].plot(input_tensor[:,3], label='Actual', color='indigo')
+    axs[1, 1].plot(input_tensor[:len(input_tensor)-1,3], label='Actual (Past)', color='blue')
+
+    axs[0, 0].set_title('1st Channel')
+    axs[0, 0].set_xlabel('Time')
+    axs[0, 0].set_ylabel('Price')
+    
+    axs[0, 1].set_title('2nd Channel')
+    axs[0, 1].set_xlabel('Time')
+    axs[0, 1].set_ylabel('Price')
+    
+    axs[1, 0].set_title('3rd Channel')
+    axs[1, 0].set_xlabel('Time')
+    axs[1, 0].set_ylabel('Price')
+    
+    axs[1, 1].set_title('4th Channel')
+    axs[1, 1].set_xlabel('Time')
+    axs[1, 1].set_ylabel('Price')
+    
+    axs[0, 0].grid()
+    axs[0, 1].grid()
+    axs[1, 0].grid()
+    axs[1, 1].grid()
+
+    axs[0, 0].legend()
+    axs[0, 1].legend()
+    axs[1, 0].legend()
+    axs[1, 1].legend()
     plt.show()
 
 
-
 if __name__ == "__main__":
-    main()
+    main(True)